f31bf031ee320dc27aaf897c01ebf2073ea0ccf2
Kevin Rose <profkevinrose@gmail.com>
2024-11-10 11:51:37 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 let config = {
-  apiKey: "xxx",
-  authDomain: "bezkoder-firebase.firebaseapp.com",
+  apiKey: "AIzaSyCR7iutXvcbe9_vF9p83roSqBzo3ioxFJs",
+  authDomain: "node-firebase-55a66.firebaseapp.com",
   databaseURL: "https://bezkoder-firebase.firebaseio.com",
-  projectId: "bezkoder-firebase",
-  storageBucket: "bezkoder-firebase.appspot.com",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+  projectId: "node-firebase-55a66",
+  storageBucket: "node-firebase-55a66.firebasestorage.app",
+  messagingSenderId: "401812243089",
+  appId: "1:401812243089:web:0b9a623f73d3d80d52f530",
 };
 
 firebase.initializeApp(config);
